4602a291656eafb04e0f81892a29927fee99fa43
Danail Slavov <danslavov@gmail.com>
2017-08-03 20:55:56 +0300

    Add files via upload

---
+++ 07_JS-Calculator/controllers/home.js
@@ -0,0 +1,20 @@
+const Calculator = require("./../models/Calculator");
+
+module.exports = {
+    indexGet: (req, res) => {
+        res.render('home/index');
+    },
+    indexPost: (req, res) => {
+        let calculatorBody = req.body;
+        let calculatorParams = calculatorBody["calculator"];
+
+        let calculator = new Calculator();
+        calculator.rightOperand = Number(calculatorParams.rightOperand);
+        calculator.leftOperand = Number(calculatorParams.leftOperand);
+        calculator.operator = calculatorParams.operator;
+
+        let result = calculator.calculateResult();
+
+        res.render("home/index", {"calculator": calculator, "result": result});
+    }
+};
