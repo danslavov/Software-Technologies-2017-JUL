79e9af71fc9ffbfb24430d2ed796119c1196ba0d
Danail Slavov <danslavov@gmail.com>
2017-09-01 17:03:58 +0300

    Add files via upload

---
+++ 13_ExamPreparation/Kanban_CS/TeisterMask/Controllers/TaskController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using TeisterMask.Models;
+
+namespace TeisterMask.Controllers
+{
+        [ValidateInput(false)]
+	public class TaskController : Controller
+	{
+	    [HttpGet]
+		[Route("")]
+	    public ActionResult Index()
+	    {
+			using (var db = new TeisterMaskDbContext())
+			{
+				var tasks = db.Tasks.ToList();
+				return View(tasks);
+			}
+		}
+
+		[HttpGet]
+		[Route("create")]
+		public ActionResult Create()
+		{
+			var tempTask = new Task();
+			tempTask.Status = "Open";
+			tempTask.Title = "";
+			return View(tempTask);
+		}
+
+		[HttpPost]
+		[Route("create")]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(Task task)
+		{
+			if (ModelState.IsValid)
+			{
+				using (var db = new TeisterMaskDbContext())
+				{
+					db.Tasks.Add(task);
+					db.SaveChanges();
+				}
+				return RedirectToAction("Index");
+			}
+			return View(task);
+		}
+
+		[HttpGet]
+		[Route("edit/{id}")]
+		public ActionResult Edit(int? id)
+		{
+			if (id != null)
+			{
+				using (var db = new TeisterMaskDbContext())
+				{
+					var taskToEdit = db.Tasks.Find(id);
+					if (taskToEdit != null)
+					{
+						return View(taskToEdit);
+					}
+				}
+			}
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		[Route("edit/{id}")]
+		[ValidateAntiForgeryToken]
+		public ActionResult EditConfirm(int? id, Task taskModel)
+		{
+			if (ModelState.IsValid && id != null)
+			{
+				using (var db = new TeisterMaskDbContext())
+				{
+					var taskToEdit = db.Tasks.Find(id);
+					if (taskToEdit != null)
+					{
+						taskToEdit.Status = taskModel.Status;
+						taskToEdit.Title = taskModel.Title;
+						db.SaveChanges();
+					}
+				}
+				return RedirectToAction("Index");
+			}
+			return View("Edit", taskModel);
+		}
+	}
+}
---
+++ 13_ExamPreparation/Kanban_CS/TeisterMask/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace TeisterMask.Models
+{
+    public class Task
+    {
+		[Key]
+	    public int Id { get; set; }
+
+		[Required]
+		[AllowHtml]
+		[MinLength(2), MaxLength(30)]
+		public string Title { get; set; }
+
+		[Required]
+		[AllowHtml]
+		[MinLength(4), MaxLength(15)]
+		public string Status { get; set; }
+	}
+}
